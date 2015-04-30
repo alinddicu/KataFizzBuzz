c0732822076e0ae23a7a1a0aa95344566300488a
alinddicu <alinddicu@yahoo.com>
2015-05-01 00:15:14 +0200

    red test

---
+++ KataFizzBuzz/KataFizzBuzz.Test/FizzBuzzer.cs
@@ -0,0 +1,27 @@
+﻿namespace KataFizzBuzz.Test
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    public class FizzBuzzer
+    {
+        public IEnumerable<string> Execute(int limit)
+        {
+            for (var index = 1; index <= limit; index++)
+            {
+                if (index % 3 == 0)
+                {
+                    yield return "Fizz";
+                }
+                else if (index % 5 == 0)
+                {
+                    yield return "Buzz";
+                }
+                else
+                {
+                    yield return index.ToString(CultureInfo.InvariantCulture);
+                }
+            }
+        }
+    }
+}
--- KataFizzBuzz/KataFizzBuzz.Test/UnitTest1.cs
+++ KataFizzBuzz/KataFizzBuzz.Test/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿namespace KataFizzBuzz.Test
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
 
     [TestClass]
     public class UnitTest1
@@ -8,6 +9,11 @@
         [TestMethod]
         public void TestMethod1()
         {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Execute(10);
+
+            Check.That(result).ContainsExactly("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", " Buzz");
         }
     }
 }
