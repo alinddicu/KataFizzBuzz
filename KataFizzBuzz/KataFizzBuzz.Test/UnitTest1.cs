ca0ac1c87b4ba067ede85add4755f18f76d7bca9
alinddicu <alinddicu@yahoo.com>
2015-05-01 00:01:56 +0200

    init

---
+++ KataFizzBuzz/KataFizzBuzz.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace KataFizzBuzz.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
